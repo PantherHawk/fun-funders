3c4c7cee38bf42112e414ccbc48c6aa86d923fcc
Alexander Rosenthal <alexrosenthalster@gmail.com>
2018-01-31 20:54:27 -0500

    feat/ have cleanSSN slice them first nine nums out that uber long string

--- python/src.py
+++ python/src.py
@@ -3,12 +3,13 @@ import re
 def cleanSSN( str ):
     # remove dashes and non-numbers
     str = re.sub("[^0-9]", "", str)
-    # if length of ssn less than nine
-    if len(str) < 9:
-        # add zeroes to front of ssn until length is 9
+    # if length of ssn less than nine add zeroes to front of ssn until length is 9
+    while len(str) < 9:
         str = '0' + str
     # if length of ssn greater than nine
+    if len(str) > 9:
      # slice first nine numbers
+     str = str[:10]
     # reformat by taking slice of first three numbers (0, 3), slice of next two (3, 5), and the last four (5)
     print(str)
     return;
