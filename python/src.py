7beb687728ab04d4fd49a3528dcbba4d1bbd9855
Alexander Rosenthal <alexrosenthalster@gmail.com>
2018-01-31 20:49:02 -0500

    feat/ ssnCleaner adds zeroes to front of strings of length less than nine

--- python/src.py
+++ python/src.py
@@ -4,7 +4,9 @@ def cleanSSN( str ):
     # remove dashes and non-numbers
     str = re.sub("[^0-9]", "", str)
     # if length of ssn less than nine
+    if len(str) < 9:
         # add zeroes to front of ssn until length is 9
+        str = '0' + str
     # if length of ssn greater than nine
      # slice first nine numbers
     # reformat by taking slice of first three numbers (0, 3), slice of next two (3, 5), and the last four (5)
